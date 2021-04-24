3b24fad4e31cb3120a50746ae3313b685e1965d5
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-04-24 18:08:09 -0400

    Initial project setup and layouting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <div class="container mx-auto">
+    <router-view /> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from '@/components/Header'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      Header
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <main v-if="!loading">
+    Show Data
+  </main>
+  <main class= "flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-500 m-auto" alt=""/>
+  </main>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries:  [],
+      loadingImage: require('../assets/orb.gif')
+    }
+  },
+  methods: {
+   async fetchCovidData() {
+     const res = await fetch('https://api.covid19api.com/summary')
+     const data = await res.json()
+     return data
+   }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
+  
+}
+</script>
