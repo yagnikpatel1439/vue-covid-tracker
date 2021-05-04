c7a0115c066c8bd98297b161f79613218e6f1fcd
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-05-04 16:16:07 -0400

    Working to select Country

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,33 @@
+<template>
+    <select 
+    v-model="selected" 
+    class="form-select mt-10 block border w-full p-3 rounded"
+    @change="onChange()"
+    >
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :key="country.ID" >
+            {{ country.Country }}
+        </option>
+    </select>
+</template>
+
+<script>
+
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) => item.ID === this.selected)
+            // console.log(country)
+            this.$emit('get-country', country)
+        }
+    }
+
+}
+</script>
